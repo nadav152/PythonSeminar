45b315a2d4eaa63fd086403f8ad2b8199f2e6707
meronleshem <meronleshem10@gmail.com>
2021-05-24 13:02:48 +0300

    Fixed no piece can move issue

--- model.py
+++ model.py
@@ -259,6 +259,11 @@ class Model:
         return None
 
     def check_no_piece_can_move(self, color):
+        """"
+        draw red circles on squares the player can move on
+        :param color: player's turn
+        :return: True if no possible moves to any piece -> auto lose.
+        """
         if self.white_left == 0 or self.black_left == 0:
             return False
         if self.black_left > 5 and color is BLACK:
